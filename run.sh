cc951aa0763b47f77f6fbcaffec6d65673d1b005
Noor Mohammed <noormohammedb@protonmail.com>
2023-06-15 21:12:53 +0200

    run script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cargo b -r
+# sudo setcap cap_net_admin=eip $CARGO_TARGET_DIR/release/trust
+# sudo setcap cap_net_admin=eip $CARGO_TARGET_DIR/debug/trust
+# $CARGO_TARGET_DIR/release/trust &
+
+sudo setcap cap_net_admin=eip $PWD/target/release/trust
+sudo setcap cap_net_admin=eip $PWD/target/debug/trust
+$PWD/target/release/trust &
+pid=$!
+sudo ip addr add 192.168.0.1/24 dev tun0 
+sudo ip link set up dev tun0
+wait $pid
