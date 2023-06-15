9b211226638662b30f00137c1c7cf8c2a34d46a9
Noor Mohammed <noormohammedb@protonmail.com>
2023-06-15 21:50:54 +0200

    listening with loop

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use tun_tap;
 fn main() -> Result<(), io::Error> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tap)?;
     let mut buf = [0u8; 1504];
-    let nbytes = nic.recv(&mut buf[..])?;
-    eprintln!("read {} bytes: {:?}", nbytes, &buf[..nbytes]);
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        eprintln!("read {} bytes: {:?}", nbytes, &buf[..nbytes]);
+    }
     Ok(())
 }
