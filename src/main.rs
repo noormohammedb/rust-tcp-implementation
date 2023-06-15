b346eddf4947adecd675c99f7c68850426e6aa0b
Noor Mohammed <noormohammedb@protonmail.com>
2023-06-15 21:01:45 +0200

    basic tap setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use tun_tap;
+
+fn main() -> Result<(), io::Error> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tap)?;
+    let mut buf = [0u8; 1504];
+    let nbytes = nic.recv(&mut buf[..])?;
+    eprintln!("read {} bytes: {:?}", nbytes, &buf[..nbytes]);
+    Ok(())
 }
