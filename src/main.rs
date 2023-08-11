3ee31f2c2fcc02f5a63243a03ae937c105a9884b
Noor Mohammed B <noormohammedb@protonmail.com>
2023-08-11 23:40:02 +0400

    parsing tcp headers sample

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io;
 use tun_tap;
 
 const ETHERTYPE_IPV4: u16 = 0x0800;
+const TCP_PROTOCOL: u8 = 0x06;
 
 fn main() -> Result<(), io::Error> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tap)?;
@@ -20,16 +21,28 @@ fn main() -> Result<(), io::Error> {
 
         match etherparse::Ipv4HeaderSlice::from_slice(&buf[ip_header_offset..nbytes]) {
             Ok(packet) => {
+                let ip_buf = &buf[ip_header_offset..nbytes];
+
                 let packet_version = packet.version();
                 let src = packet.source();
                 let dst = packet.destination();
                 let proto = packet.protocol();
                 let t_len = packet.total_len();
                 let pay_len = packet.payload_len();
+                if proto != TCP_PROTOCOL {
+                    println!("skipping a non tcp packet, proto: {:?}", proto);
+                    continue;
+                }
+
+                println!("{:#x?}", ip_buf);
+
+                let src_port = u16::from_be_bytes([ip_buf[20], ip_buf[21]]);
+                let dst_port = u16::from_be_bytes([ip_buf[22], ip_buf[23]]);
+
                 eprintln!(
-                    "read {} bytes (flags: {:x}, e_proto: {:x}) v: {:?}, src: {:?}, dst: {:?}, proto: {:?}, t_len: {:?},p_len: {:?}",
+                    "read {} bytes (flags: {:x}, e_proto: {:x}) v: {:?}, src: {:?}:{:?}, dst: {:?}:{:?}, proto: {:?}, t_len: {:?},p_len: {:?}",
                     nbytes - 4,
-                    ether_flags, ether_proto, packet_version, src, dst,proto,t_len,pay_len
+                    ether_flags, ether_proto, packet_version, src,src_port, dst,dst_port,proto,t_len,pay_len
                 );
             }
             Err(e) => eprintln!("ignoring a packet {:?}", e),
