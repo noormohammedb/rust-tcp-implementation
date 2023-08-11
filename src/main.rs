23f0c76e4f9f2cca66761635c809b63e56479d2d
Noor Mohammed B <noormohammedb@protonmail.com>
2023-08-12 01:23:37 +0400

    parsing tcp header with etherparse

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,11 @@ fn main() -> Result<(), io::Error> {
             continue;
         }
         let ip_header_offset = 18; // size of the ethernet
+        let ip_header_size = 20; // min ip header size
 
         match etherparse::Ipv4HeaderSlice::from_slice(&buf[ip_header_offset..nbytes]) {
             Ok(packet) => {
+                println!("{:?}\n", packet.to_header());
                 let ip_buf = &buf[ip_header_offset..nbytes];
 
                 let packet_version = packet.version();
@@ -28,21 +30,28 @@ fn main() -> Result<(), io::Error> {
                 let dst = packet.destination();
                 let proto = packet.protocol();
                 let t_len = packet.total_len();
-                let pay_len = packet.payload_len();
+                let p_len = packet.payload_len();
                 if proto != TCP_PROTOCOL {
                     println!("skipping a non tcp packet, proto: {:?}", proto);
                     continue;
                 }
 
-                println!("{:#x?}", ip_buf);
+                // let src_port = u16::from_be_bytes([ip_buf[20], ip_buf[21]]);
+                // let dst_port = u16::from_be_bytes([ip_buf[22], ip_buf[23]]);
 
-                let src_port = u16::from_be_bytes([ip_buf[20], ip_buf[21]]);
-                let dst_port = u16::from_be_bytes([ip_buf[22], ip_buf[23]]);
+                // println!("{:x?}", ip_buf);
+
+                match etherparse::TcpHeader::from_slice(&ip_buf[ip_header_size..]) {
+                    Ok(tcp_header) => {
+                        println!("{:?}\n", tcp_header);
+                    }
+                    Err(e) => eprintln!("tcp header parse error: {:?}", e),
+                }
 
                 eprintln!(
-                    "read {} bytes (flags: {:x}, e_proto: {:x}) v: {:?}, src: {:?}:{:?}, dst: {:?}:{:?}, proto: {:?}, t_len: {:?},p_len: {:?}",
+                    "read {} bytes (flags: {:x}, e_proto: {:x}) v: {:?}, src: {:?}, dst: {:?}, proto: {:?}, t_len: {:?},p_len: {:?}\n\n",
                     nbytes - 4,
-                    ether_flags, ether_proto, packet_version, src,src_port, dst,dst_port,proto,t_len,pay_len
+                    ether_flags, ether_proto, packet_version, src, dst, proto, t_len, p_len
                 );
             }
             Err(e) => eprintln!("ignoring a packet {:?}", e),
